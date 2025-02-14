bb49510154e1231eecf41217503043a3acda686b
Umer Qadoos <umerqadoos74@gmail.com>
2025-02-14 17:49:48 +0500

    update commit

--- src/components/layout/vertical/Navigation.jsx
+++ src/components/layout/vertical/Navigation.jsx
@@ -12,7 +12,6 @@ import { styled, useTheme } from '@mui/material/styles'
 // Component Imports
 import VerticalNav, { NavHeader } from '@menu/vertical-menu'
 import VerticalMenu from './VerticalMenu'
-import Logo from '@components/layout/shared/Logo'
 
 // Hook Imports
 import useVerticalNav from '@menu/hooks/useVerticalNav'
@@ -20,6 +19,7 @@ import useVerticalNav from '@menu/hooks/useVerticalNav'
 // Style Imports
 import navigationCustomStyles from '@core/styles/vertical/navigationCustomStyles'
 
+// Styled Box for Shadow (used for scroll effect)
 const StyledBoxForShadow = styled('div')(({ theme }) => ({
   top: 60,
   left: -8,
@@ -44,34 +44,35 @@ const Navigation = () => {
   // Refs
   const shadowRef = useRef(null)
 
+  // Scroll menu handler for adding/removing shadow on scroll
   const scrollMenu = (container, isPerfectScrollbar) => {
     container = isBreakpointReached || !isPerfectScrollbar ? container.target : container
 
     if (shadowRef && container.scrollTop > 0) {
-      // @ts-ignore
       if (!shadowRef.current.classList.contains('scrolled')) {
-        // @ts-ignore
         shadowRef.current.classList.add('scrolled')
       }
     } else {
-      // @ts-ignore
       shadowRef.current.classList.remove('scrolled')
     }
   }
 
   return (
-    // eslint-disable-next-line lines-around-comment
-    // Sidebar Vertical Menu
     <VerticalNav customStyles={navigationCustomStyles(theme)} className='shadow-lg'>
-      {/* Nav Header including Logo & nav toggle icons  */}
+      {/* Nav Header including Logo & nav toggle icons */}
       <NavHeader>
-        <Link href='/' className='font-bold text-[20px] '>
-          {/* <Logo /> */}
-          Bait-al-Ne'ma
+        <Link href='/' className='font-bold text-[20px]'>
+          {/* Logo can be added later */}
+          Bait-al-Ne&apos;ma {/* Updated apostrophe */}
         </Link>
+        {/* Close icon for mobile breakpoint */}
         {isBreakpointReached && <i className='ri-close-line text-xl' onClick={() => toggleVerticalNav(false)} />}
       </NavHeader>
+
+      {/* Shadow effect for scroll */}
       <StyledBoxForShadow ref={shadowRef} />
+
+      {/* Vertical Menu */}
       <VerticalMenu scrollMenu={scrollMenu} />
     </VerticalNav>
   )
